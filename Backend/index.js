8573a87e5962e64e796a558c1c6896d142d05e40
Saraswati121 <99686266+Saraswati121@users.noreply.github.com>
2022-11-02 00:55:48 +0530

    authentication part done

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connect = require("./config/db")
+const authentication = require("./controller/auth")
+const quizRouter = require("./controller/quiz");
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+
+app.use("/auth",authentication)
+app.use("/quiz",quizRouter)
+
+const port = process.env.PORT || 8080;
+
+app.listen(port,async()=>{
+    await connect()
+    console.log('listening on port 8080');
+})
